00a3671003bd1e2e03a4e8ea7a275b70aa2c932e
seungyeon <seungyeon@218.38.137.27>
2021-03-01 22:42:47 +0900

    Hello, World!

---
+++ helloworldapp.java
@@ -0,0 +1,9 @@
+public class helloworldapp 
+			//클래스명 'helloworldapp' 는 생성한 파일명과 같아야한다.
+{
+	public static void main(String[] args) 
+						//'main'에 코드 작성!
+	{
+		System.out.println("Hello, World!");
+	}
+}
